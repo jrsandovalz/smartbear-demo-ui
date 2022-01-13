68dfff8f2f53fd5dfbf5bdf5568980c644eb4965
dchavezwaverley <dchavez@waverleysoftware.com>
2022-01-13 12:14:51 -0400

    Adding Login page

---
+++ smartbear-demo/src/components/login/login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import PropTypes from 'prop-types';
+
+export default function login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+  
+    const handleSubmit = async e => {
+      e.preventDefault();
+      setToken("abc");
+    }
+  
+
+    return (
+        <>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" onChange={e => setUserName(e.target.value)}/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </>
+    );
+}
+login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
