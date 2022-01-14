410e26081406e4eb79486507a115daae54feddfa
dchavezwaverley <dchavez@waverleysoftware.com>
2022-01-14 08:49:50 -0400

    Adding styles and redirection

--- smartbear-demo/src/components/login/LoginSmart.js
+++ smartbear-demo/src/components/login/LoginSmart.js
@@ -2,31 +2,33 @@ import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+
+import "./login.css"
 
 export default function Login({ setToken }) {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
-  
+
     const handleSubmit = async e => {
-      setToken("abc");
+        e.preventDefault();
+        setToken("abc");
+        window.location.replace('/');
     }
-  
+
 
     return (
-        <Container>
-            <Form onSubmit={handleSubmit}>
+        <Container className='login-container'>
+            <Form onSubmit={handleSubmit} className='d-grid gap-2'>
                 <Form.Group className="mb-3" controlId="formBasicUsername">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Enter username" onChange={e => setUserName(e.target.value)}/>
+                    <Form.Control type="text" placeholder="Username" onChange={e => setUserName(e.target.value)} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+                <Button variant="primary" type="submit" size='lg'>
+                    Log In
                 </Button>
             </Form>
         </Container>
